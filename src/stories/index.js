1af0e0abb60769c9d0bc132c7a334c583b8e8ff4
RONENLA <ronen.laufer@amdocs.com>
2017-11-29 21:13:28 +0200

    added dynamic props using knobs

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import CoolTextArea from '../components/CoolTextArea';
 import '../index.css';
 import '../App.css';
 
 
 storiesOf('CoolTextArea', module)
-    .add('with minimum 4 rows and maximum 10 rows', () => <CoolTextArea minrows={4} maxrows={10} />);
+    .addDecorator(withKnobs)
+    .add('with dynamic props', () =>
+        <CoolTextArea minrows={number('Minimum Rows', 4)} maxrows={number('Maximum Rows', 10)} />);
